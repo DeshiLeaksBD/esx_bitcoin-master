2d8683b1b015f145deb4b1a2cc74fdb09195bb8a
DeshiLeaksBD <faiyazsadik02@gmail.com>
2021-06-03 09:50:45 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# esx_bitcoin-master
+ This is a bitcoin Script for ESX server. posted by Deshi Leaks BD
---
+++ locales/en.lua
@@ -0,0 +1,10 @@
+Locales['en'] = {
+	['carry_to_pick_fruit'] = 'Press ~INPUT_CONTEXT~ to catch Bitcoin',
+	['carry_to_sell_the_bags'] = 'Press ~INPUT_CONTEXT~ to sell the BitCoin',
+	['backpack_full'] = 'You can not get more Bicoin because your ~r~schoolbag~s~',
+	['get_fruit'] = '~y~A Minerar Bitcoin~s~...',
+	['you dont_have_more_fruits'] = 'You Do not Have More ~r~Bicoin~s~',
+	['dont_have_sacks_with_fruits'] = 'You Do not Have More ~r~Bitcoin~s~',
+	['sold_bags'] = 'Sell ~y~x1~s~ ~y~Bitcoin~s~',
+	['bag_sale'] = '~g~For sale Bitcoin~s~...',
+}
